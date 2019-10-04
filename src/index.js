24567e9a68ccdab58128a8ef397655b05d942ee8
Vitaly Raicheu <RVitaly1978@gmail.com>
2019-10-04 18:55:08 +0200

    fix: add calculation with division by 2

--- src/index.js
+++ src/index.js
@@ -2,24 +2,36 @@ module.exports = function zeros(expression) {
   // your solution
   let str = expression;
   let str_array = str.split("*");
-  let number_zeros = 0;
+  let sum5 = 0;
+  let sum2 = 0;
   str_array.forEach(element => {
     let number = element.replace(/!/g, "");
     let i = element.length - number.length;
     let n = Number(number);
-    let zeros_local = 0;
+    let number5 = 0;
+    let number2 = 0;
     if (i === 1) {
       for (let j = n; j >= 5; j--) {
-        zeros_local += ((j % 5 != 0) ? 0 : 1 + ((j % 25 != 0) ? 0 : 1) + ((j % 125 != 0) ? 0 : 1));
+        number5 += ((j % 5 != 0) ? 0 : 1 + ((j % 25 != 0) ? 0 : 1) + ((j % 125 != 0) ? 0 : 1));
       };
     } else {
       for (let j = n; j >= 5; j = j - 2) {
-        zeros_local += ((j % 5 != 0) ? 0 : 1 + ((j % 25 != 0) ? 0 : 1) + ((j % 125 != 0) ? 0 : 1));
+        number5 += ((j % 5 != 0) ? 0 : 1 + ((j % 25 != 0) ? 0 : 1) + ((j % 125 != 0) ? 0 : 1));
       };
     };
-    number_zeros += zeros_local;
+    if (i === 1) {
+      for (let j = n; j >= 2; j--) {
+        number2 += ((j % 2 != 0) ? 0 : 1 + ((j % 4 != 0) ? 0 : 1) + ((j % 8 != 0) ? 0 : 1) + ((j % 16 != 0) ? 0 : 1) + ((j % 32 != 0) ? 0 : 1) + ((j % 64 != 0) ? 0 : 1) + ((j % 128 != 0) ? 0 : 1));
+      };
+    } else {
+      for (let j = n; j >= 2; j = j - 2) {
+        number2 += ((j % 2 != 0) ? 0 : 1 + ((j % 4 != 0) ? 0 : 1) + ((j % 8 != 0) ? 0 : 1) + ((j % 16 != 0) ? 0 : 1) + ((j % 32 != 0) ? 0 : 1) + ((j % 64 != 0) ? 0 : 1) + ((j % 128 != 0) ? 0 : 1));
+      };
+    };
+    sum5 += number5;
+    sum2 += number2;
   });
-  return number_zeros;
+  return Math.min(sum5, sum2);
 }
 // function factorial(n) {
   //   return (n != 1) ? n * factorial(n - 1) : 1;
